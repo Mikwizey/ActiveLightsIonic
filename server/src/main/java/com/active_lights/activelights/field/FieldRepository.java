52abe34c3efee10a822b05f7bfb03e85f7f73fdd
Madeleine <mikaelsson.madeleine@gmail.com>
2018-04-24 16:38:34 +0200

    Moved files and deleted server

--- server/src/main/java/com/active_lights/activelights/field/FieldRepository.java
+++
@@ -1,7 +0,0 @@
-package com.active_lights.activelights.field;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-interface FieldRepository extends JpaRepository<Field, Long> {}
