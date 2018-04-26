8174cdc206f408ba986ba6b1c5150a4468747350
suselisan <suselisan840@gmail.com>
2018-04-26 12:46:04 +0200

    Commit förändringar som gjorts så gps position kan ses

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, ToastController } from 'ionic-angular';
+import { NavController, NavParams, ToastController, Platform } from 'ionic-angular';
 import {FieldService} from "../../providers/field-service";
+
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 @Component({
   selector: 'page-home',
@@ -8,8 +11,27 @@ import {FieldService} from "../../providers/field-service";
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public fieldService: FieldService) {
 
-  }
+  constructor(public navCtrl: NavController, public fieldService: FieldService, private geolocation: Geolocation, private platform: Platform) {
+    this.platform.ready().then(() => {
 
+      var options = {
+        timeout: 5000
+      };
+
+
+      this.geolocation.getCurrentPosition(options).then(resp => {
+        console.log(resp.coords.latitude);
+        console.log(resp.coords.longitude);
+      }).catch((error) =>{
+      console.log("Error can't find the location", error);
+      });
+
+      let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+        console.log(data.coords.latitude);
+        console.log(data.coords.longitude);
+});
+  });
+}
 }
