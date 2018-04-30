60e5d56969e26dfa5bda4efcdea92749184f99fa
Madeleine <mikaelsson.madeleine@gmail.com>
2018-04-30 09:34:45 +0200

    Map now centers on current position and marks it

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,8 @@
-
-import { Geolocation } from '@ionic-native/geolocation';
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController, NavParams, Platform } from 'ionic-angular';
-import { FieldService } from "../../providers/field-service";
-import { ListPage } from '../list/list';
+import {Geolocation} from '@ionic-native/geolocation';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {NavController, Platform} from 'ionic-angular';
+import {FieldService} from "../../providers/field-service";
+import {ListPage} from '../list/list';
 
 declare var google: any;
 
@@ -17,30 +16,9 @@ declare var google: any;
 export class HomePage {
 
 
-    @ViewChild('map') mapRef: ElementRef;
-    
-  constructor(public navCtrl: NavController, public fieldService: FieldService, private geolocation: Geolocation, private platform: Platform) {
-    this.platform.ready().then(() => {
+  @ViewChild('map') mapRef: ElementRef;
 
-        var options = {
-            timeout: 5000
-        };
-
-
-        this.geolocation.getCurrentPosition(options).then(resp => {
-            console.log(resp.coords.latitude);
-            console.log(resp.coords.longitude);
-        }).catch((error) =>{
-            console.log("Error can't find the location", error);
-        });
-
-
-        let watch = this.geolocation.watchPosition();
-        watch.subscribe((data) => {
-            console.log(data.coords.latitude);
-            console.log(data.coords.longitude);
-        });
-    });
+  constructor(public navCtrl: NavController, public fieldService: FieldService, private geolocation: Geolocation, ) {
   };
 
 
@@ -50,80 +28,95 @@ export class HomePage {
 
 
   showMap() {
-    // Map options
-    var optionsMap = {
-      zoom: 11,
-      center: { lat: 59.334591, lng: 18.063240 }
-    }
 
-    // New map
-    var map = new google.maps.Map
+    this.geolocation.getCurrentPosition().then(resp => {
+      console.log(resp.coords.latitude);
+      console.log(resp.coords.longitude);
 
-    (this.mapRef.nativeElement, optionsMap);
+      // Map options
+      let optionsMap = {
+        zoom: 15,
+        center: {lat: resp.coords.latitude, lng: resp.coords.longitude}
+      };
 
-    // Listen for click on map
-    google.maps.event.addListener(map, 'click', function (event) {
-      // Add marker
-      addMarker({ coords: event.latLng });
-    });
+      // New map
+      let map = new google.maps.Map
 
-    // Array of markers
-    var markers = [
-      {
-        coords: { lat: 59.334591, lng: 18.063240 },
-        iconImage: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
-        content: '<h1>Johannes Bollplan</h1>'
-      },
-      {
-        coords: { lat: 59.3584, lng: 18.100 },
-        iconImage: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-        content: '<h1>Bergholms Bollplan</h1>'
+      (this.mapRef.nativeElement, optionsMap);
 
-      },
-      {
-
-        coords: { lat: 59.3684, lng: 18.052240 },
-        iconImage: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
-      }
-    ];
-
-    // Loop through markers
-    for (var i = 0; i < markers.length; i++) {
-      // Add marker
-      addMarker(markers[i]);
-    }
-
-    // Add Marker Function
-    function addMarker(props) {
-      var marker = new google.maps.Marker({
-        position: props.coords,
+      //Mark current position
+      let currentPos = new google.maps.Marker({
+        position: {lat: resp.coords.latitude, lng: resp.coords.longitude},
         map: map,
-        //icon:props.iconImage
+        icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
       });
 
-      // Check for marker image
-      if (props.iconImage) {
-        // Set icon image
-        marker.setIcon(props.iconImage);
+      // Listen for click on map
+      google.maps.event.addListener(map, 'click', function (event) {
+        // Add marker
+        addMarker({coords: event.latLng});
+      });
+
+      // Array of markers
+      let markers = [
+        {
+          coords: {lat: 59.334591, lng: 18.063240},
+          iconImage: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+          content: '<h1>Johannes Bollplan</h1>'
+        },
+        {
+          coords: {lat: 59.3584, lng: 18.100},
+          iconImage: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+          content: '<h1>Bergholms Bollplan</h1>'
+
+        },
+        {
+
+          coords: {lat: 59.3684, lng: 18.052240},
+          iconImage: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
+        }
+      ];
+
+      // Loop through markers
+      for (let i = 0; i < markers.length; i++) {
+        // Add marker
+        addMarker(markers[i]);
       }
 
-      // Check content
-      if (props.content) {
-        var infoWindow = new google.maps.InfoWindow({
-          content: props.content
+      // Add Marker Function
+      function addMarker(props) {
+        let marker = new google.maps.Marker({
+          position: props.coords,
+          map: map,
+          //icon:props.iconImage
         });
 
-        marker.addListener('click', function () {
-          infoWindow.open(map, marker);
-        });
+        // Check for marker image
+        if (props.iconImage) {
+          // Set icon image
+          marker.setIcon(props.iconImage);
+        }
+
+        // Check content
+        if (props.content) {
+          let infoWindow = new google.maps.InfoWindow({
+            content: props.content
+          });
+
+          marker.addListener('click', function () {
+            infoWindow.open(map, marker);
+          });
+        }
       }
-    }
+    }).catch((error) => {
+      console.log("Error can't find the location", error);
+    });
+  }// ShowMap
+
+
+  goToList() {
+
+    this.navCtrl.push(ListPage);
   }
-
-
-    goToList(){
-
-        this.navCtrl.push(ListPage);
-    }
 
 }
