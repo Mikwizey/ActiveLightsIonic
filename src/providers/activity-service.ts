fba4b4f19e0431b6b77b2b7bfb95d5704034e7aa
Ella <ellaelonen@gmail.com>
2018-05-13 11:40:44 +0200

    ActivityService

--- src/providers/activity-service.ts
+++ src/providers/activity-service.ts
@@ -2,24 +2,25 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { BrowserModule} from'@angular/platform-browser';
+import { DateTime } from 'ionic-angular';
 
 @Injectable()
 export class ActivityService {
     public API = 'https://pvt.dsv.su.se/Group02';
-    public ACTIVITY_API = this.API + '/field/{id}/activity/add';
+    public ACTIVITY_API = this.API;
     constructor(public http: HttpClient) {
 
     }
     getAllActivities(): Observable<any> {
-        return this.http.get(this.API + "/field/{id}/activity");
+        return this.http.get(this.API + "/field/" + "/activity/",);
       }
-    addAllActivities(id): Observable<any> {
-      return this.http.put(this.API + "/field/"+id+"/activity/"+"add",{});
+    addActivity(id): Observable<any> {
+      return this.http.post(this.API + "/field/" + id + "/activity/add",{title: String, description : String, 
+        startTime: DateTime, endTime : DateTime}); 
       }
       
       getActivityByID(): Observable<any> {
-          return this.http.get(this.API + "/field/{id}/activity/{aid}");
+          return this.http.get(this.API + "/field/" + "/activity/", {});
       }
-
 
 }
